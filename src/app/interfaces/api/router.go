b54e780d9352317ce26edf9fd0c9cb53fa34ce2b
Kazuhiro Ike <ganbare.carp@gmail.com>
2020-09-01 08:53:19 +0900

    Add API server starting script

---
+++ src/app/interfaces/api/router.go
@@ -0,0 +1,23 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/koifish082/golang-api-layered/src/app/library/log"
+)
+
+type App struct {
+}
+
+// NewApp factory returns an instantiated App
+func NewApp() *App {
+	return &App{}
+}
+
+func (a *App) Router() *gin.Engine {
+	router := gin.New()
+	router.GET("/welcome", func(c *gin.Context) {
+		log.Infoln("hello world!")
+	})
+	gin.Default()
+	return router
+}
